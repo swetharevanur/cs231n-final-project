a2a2e8001ea17686b2ba3f2482571d19951e5e75
Swetha Revanur <swetharevanur@gmail.com>
2018-05-15 23:03:22 -0700

    Added pause functionality during video playback

--- src/read-clips.py
+++ src/read-clips.py
@@ -8,16 +8,22 @@ video = swag.VideoCapture(fname)
 
 frameCount = 0
 
-while (True):
+while True:
 	ret, frame = video.read() 
 
 	if ret == True: # ret = False indicates EOF
 		cv2.putText(frame, "Frame " + str(frameCount), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255));
 		cv2.imshow('frame', frame)
 		frameCount += 1
+		
+		# hit 'p' key to pause and then 'p' again to play
+		if cv2.waitKey(1) == ord('p'):
+			while cv2.waitKey(1) != ord('p'): continue
+
 		# hit 'q' key to close window
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
+
 	else:
 		break
 
