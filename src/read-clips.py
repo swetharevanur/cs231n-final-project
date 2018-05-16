84d582a281949a52f52ba9b69bc03a28d500efd3
Swetha Revanur <swetharevanur@gmail.com>
2018-05-15 22:54:22 -0700

    Added frame count to video playback

--- src/read-clips.py
+++ src/read-clips.py
@@ -5,12 +5,16 @@ import numpy as np
 fname = '../../jackson-clips'
 
 video = swag.VideoCapture(fname)
- 
+
+frameCount = 0
+
 while (True):
 	ret, frame = video.read() 
 
 	if ret == True: # ret = False indicates EOF
+		cv2.putText(frame, "Frame " + str(frameCount), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255));
 		cv2.imshow('frame', frame)
+		frameCount += 1
 		# hit 'q' key to close window
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
