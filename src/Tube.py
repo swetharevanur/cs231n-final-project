81b856d4f2aea37a078230e1c0072eb53efda515
Swetha Revanur <swetharevanur@gmail.com>
2018-05-16 17:50:17 -0700

    Zero-padded frames in tube; started spatial max pooling

--- src/Tube.py
+++ src/Tube.py
@@ -1,10 +1,14 @@
+# Creates normalized actions tubes for objects from bounding box data
 import swag 
 import cv2
+from operator import itemgetter
+import math
 
 class Tube():
 	def __init__(self, obj_id):
 		self.id = obj_id
 		self.rois = [] # list of regions of interest
+		self.pooled_rois = [] # RoIs after spatial max pooling across all frames
 
 	def populate(self, bb):
 		frames_of_interest = bb.loc[bb['ind'] == self.id]
@@ -21,7 +25,60 @@ class Tube():
 			ret, frame = video.read()
 			if ret == False: break # EOF reached
 
-			roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)]
+			roi = frame[int(y_min):int(y_max), int(x_min):int(x_max)] # crop
 			self.rois.append(roi)
 
 		video.release()
+
+	def display(self):
+		for frame in self.rois:
+			cv2.imshow('frame', frame)
+			cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
+	def spatial_pooling(self):
+		new_dim = 20
+		pool_kernel_dim = 4
+		stride = 4
+
+		for frame in self.rois:
+			frame_height, frame_width, frame_channel = frame.shape
+			print(frame_height, frame_width)
+
+			# zero pad each frame so that they all have height/width that is a multiple of new_dim
+			# multiple must be larger than frame dims
+			vert_pad = (frame_height // new_dim + 1)*new_dim - frame_height
+			horiz_pad = (frame_width // new_dim + 1)*new_dim - frame_width
+			if frame_height % new_dim == 0: vert_pad = 0
+			if frame_width % new_dim == 0: horiz_pad = 0
+
+			top = math.ceil(vert_pad/2)
+			bottom = vert_pad - top
+			left = math.ceil(horiz_pad/2)
+			right = horiz_pad - left
+
+			padded_frame = cv2.copyMakeBorder(frame, top, bottom, left, right, 
+				cv2.BORDER_CONSTANT, value = (0, 0, 0))
+
+			H, W, C = padded_frame.shape
+
+			# apply vectorized spatial max pooling
+			same_size = pool_kernel_dim == stride
+			tiles = H % pool_kernel_dim == 0 and W % pool_kernel_dim == 0
+			if same_size and tiles:
+				padded_frame = padded_frame.reshape(1, C, H // pool_kernel_dim, pool_kernel_dim,
+						   W // pool_kernel_dim, pool_kernel_dim)
+				padded_frame = padded_frame.max(axis = 3).max(axis = 4)
+			else:
+				assert "Incorrect kernel size or stride for spatial max pooling"
+
+			print(padded_frame.shape[0], padded_frame.shape[1])
+			self.pooled_rois.append(padded_frame)
+
+
+	def temporal_pooling(self):
+		return
+
+
+
+
--- src/process-tubes.py
+++ src/process-tubes.py
@@ -11,17 +11,16 @@ obj_ids = bb['ind'].unique().flatten().tolist()
 tubes = []
 
 # for each object, produce a tube
-for obj_id in obj_ids[0:2]:
+for obj_id in obj_ids[0:1]:
 	print('Creating tube for object', obj_id)
 	tube = Tube(obj_id)
 	tube.populate(bb)
 	tubes.append(tube)
 	print('Finished creating tube for object', obj_id, 'with', len(tube.rois), 'frames\n')
 
-# try displaying ROIs
-tube = tubes[1]
-for frame in tube.rois:
-	cv2.imshow('frame', frame)
-	cv2.waitKey(0)
+# perform spatial max pooling
+tubes[0].spatial_pooling()
 
-cv2.destroyAllWindows()
+# displaying ROIs
+tubes[0].display()
+
