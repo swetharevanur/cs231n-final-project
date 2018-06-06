c37d2800b7cfefe936c10891c950f5455864f7b9
VSAnimator <vishnu.sarukkai@hotmail.com>
2018-06-06 00:51:32 +0000

    Resnet testing file

--- src/resnet_encoder.py
+++ src/resnet_encoder.py
@@ -10,7 +10,6 @@ import time
 import os
 import copy
 import VideoDataset
-# from conv_autoencoder import load
 
 # using gpu
 USE_GPU = True
@@ -21,36 +20,43 @@ if USE_GPU and torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 
-print('using device:', device)	
+print('Using device:', device)	
 
-IMAGE_WIDTH = 224 # 1920
-IMAGE_HEIGHT = 224 # 1080
-NUM_CHANNELS = 3
-IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT * NUM_CHANNELS
+res18_model = models.resnet18(pretrained = True)
+res18_encoder = nn.Sequential(*list(res18_model.children())[:-1])
+res18_encoder = res18_encoder.to(device)
+torch.save(res18_encoder, 'models/res18_encoder.pth')
+
+res50_model = models.resnet50(pretrained = True)
+res50_encoder = nn.Sequential(*list(res50_model.children())[:-1])
+res50_encoder = res50_encoder.to(device)
+torch.save(res50_encoder, 'models/res50_encoder.pth')
 
 def load(fname = '../../jackson-clips'):
-	print("Starting to load data.")
-	batch_size = 128
-	train_data = VideoDataset.VideoDataset(fname = fname, transform=[transforms.ToTensor()])
-	train_loader = torch.utils.data.DataLoader(train_data, shuffle = True, \
-	    batch_size = batch_size, num_workers = 8, drop_last = True)
-	return train_loader
+       print("Starting to load data.")
+       batch_size = 128
+       train_data = VideoDataset.VideoDataset(fname = fname, transform=[transforms.ToTensor()])
+       train_loader = torch.utils.data.DataLoader(train_data, shuffle = True, \
+           batch_size = batch_size, num_workers = 8, drop_last = True)
+       return train_loader
 
-res18_model = models.resnet18(pretrained=True)
-res18_conv = nn.Sequential(*list(res18_model.children())[:-1])
 
-res18_conv = res18_conv.to(device)
 
-for param in res18_conv.parameters():
-        param.requires_grad = False
+for param in res18_encoder.parameters():
+       param.requires_grad = False
+
+for param in res50_encoder.parameters():
+	param.requires_grad = False
 
 train_loader = load()
 
 for i, (images, _) in enumerate(train_loader):
-    print(i)
-    #inputs = Variable(inputs)
-    outputs = res18_conv(images)
-    print(outputs.shape)
+       print("Processing image:", i)
+       images = images.to(device = device, dtype = dtype)
+       output_18 = res18_encoder(images)
+       output_50 = res50_encoder(images)
+       print(output_18.shape)
+       print(output_50.shape)
 
 
 
